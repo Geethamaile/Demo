67368590063dece5db3bd7beef94f865e7dc7a4a
Geethamaile <geethamaile97@gmail.com>
2023-06-29 12:24:07 +0530

    tenth commit demo

--- src/Focus/VeggietBooking.java
+++ src/Focus/VeggietBooking.java
@@ -2,22 +2,38 @@ package Focus;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 public class VeggietBooking {
-
+	 public static WebDriver driver;
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		ChromeOptions opt = new ChromeOptions();
 		opt.addArguments("--remote-allow-origins=*");
-		WebDriver driver = new ChromeDriver(opt);
+		 driver = new ChromeDriver(opt);
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 		wrapper(2000);
 		driver.findElement(By.xpath("//button[text()='ADD TO CART'][1]")).click();
-		driver.findElement(By.xpath("//button[text()='ADD TO CART'][2]")).click();
-		driver.close();
+		driver.findElement(By.xpath("(//button[text()='ADD TO CART'])[2]")).click();
+		wrapper(2000);
+		OrderBtn();
+		//driver.close();
 		
 
+	}
+	public static void OrderBtn() throws InterruptedException {
+		driver.findElement(By.xpath("//input[@type='search']")).sendKeys("Br");
+		wrapper(2000);
+		
+		driver.findElement(By.xpath("(//button[text()='ADD TO CART'])[1]")).click();
+		driver.findElement(By.xpath("(//button[text()='ADD TO CART'])[2]")).click();
+		driver.findElement(By.xpath("//a[@class='cart-icon']")).click();
+	
+		
 	}
 	public static void wrapper(long num) throws InterruptedException {
 		Thread.sleep(num);
