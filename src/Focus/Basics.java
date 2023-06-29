d71dc6e644f05d825e3e49c45857ecf3021fe830
Geethamaile <geethamaile97@gmail.com>
2023-06-29 06:49:29 +0530

    Second commit message Geetha

--- src/Focus/Basics.java
+++ src/Focus/Basics.java
@@ -5,6 +5,10 @@ public class Basics {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("Java");
+		System.out.println("Sql");
+		System.out.println("Testng");
+		System.out.println("grid");
 
 	}
 
