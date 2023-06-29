48bf40c30ffb88fdae0849876397eaca5500375b
Geethamaile <geethamaile97@gmail.com>
2023-06-29 11:41:35 +0530

    eighthcommit gitstuff

---
+++ src/Focus/FlightBooking.java
@@ -0,0 +1,25 @@
+package Focus;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+public class FlightBooking {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		ChromeOptions opt = new ChromeOptions();
+		opt.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(opt);
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		wrapper(2000);
+		driver.findElement(By.xpath("//button[text()='ADD TO CART'][1]")).click();
+		driver.close();
+		
+
+	}
+	public static void wrapper(long num) throws InterruptedException {
+		Thread.sleep(num);
+	}
+
+}
